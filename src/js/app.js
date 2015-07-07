9315358a7468ce2bb106a3f2fe9953c52419f542
Robby Sahm <robertsahm@gmail.com>
2015-07-07 15:16:27 -0400

    user input sending to db

--- src/js/app.js
+++ src/js/app.js
@@ -7,24 +7,6 @@
 (function ($, undefined) {
     "use strict";
 
-    // The template code
-    var templateSource = document.getElementById('results-template').innerHTML;
-     
-    // compile the template
-    var template = Handlebars.compile(templateSource);
-     
-    // The div/container that we are going to display the results in
-    var resultsPlaceholder = document.getElementById('results');
-     
-    var data = {
-        "person": {
-            "name": "Handlebars",
-            "last": "Demo"
-        }
-    };
-     
-    resultsPlaceholder.innerHTML = template(data);
-
 })(jQuery);
 
 
--- src/js/call-to-action.js
+++ src/js/call-to-action.js
@@ -55,20 +55,63 @@
 	    	$('#cta-form-post :input').each(function(i, e) { 
 	    		data[$(e).attr('id')] = $(e).val();
 	    	});
+
     		console.log(data);
-	  
-	      var request = $.ajax({
-	         url:  'http://localhost:1437/api/salesRegistration',
-	         // url:  'http://10.20.3.1:8400/api/salesRegistration',
-	         type: 'POST',
-	         dataType: 'JSON',
-	         async: true,
-	         data: data
-	      }).done(function(data) {
-	         console.log('done');
-	      }).fail(function(xhr, status, err) {
-	      	console.log('failed');
-	      });
+
+    		var emailRegex = /^[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,4}$/i,
+             emailCheck = emailRegex.test(data['cta-input-email']),
+             emailError = 'Enter a valid Email.',
+             firstnameCheck = data['cta-input-first-name'].length > 0,
+             firstnameError = 'Enter a first name.',
+             lastnameCheck = data['cta-input-last-name'].length > 0,
+             lastnameError = 'Enter a last name.',
+             companyCheck = data['cta-input-company'].length > 0,
+             companyError = 'Enter a company.',
+             errorCheckOverall = emailCheck && firstnameCheck && lastnameCheck && companyCheck,
+             errors = [];
+
+            console.log(errorCheckOverall, emailCheck, firstnameCheck, lastnameCheck, companyCheck);
+            
+            if (!errorCheckOverall) {
+
+		       	if (!emailCheck) {
+		       	    console.log(emailError);
+		       	}
+		       	if (!firstnameCheck) {
+		       	    console.log(firstnameError);
+		       	}
+		       	if (!lastnameCheck) {
+		       	    console.log(lastnameError);
+		       	}
+		       	if (!companyCheck) {
+		       	    console.log(companyError);
+		       	}
+		       	console.log("denied!");
+		      return;
+		    
+			} else {
+
+				console.log("sending...");
+
+			   var request = $.ajax({
+			      // url:  'http://localhost:1437/api/salesRegistration',
+			      url:  'http://10.20.3.1:8400/api/salesRegistration',
+			      type: 'POST',
+			      dataType: 'JSON',
+			      async: true,
+			      data: data
+			   }).done(function(data) {
+
+			   	if (data.success) {
+					   console.log('success!');
+					} else {
+					   console.log('error:', data.message);
+					}
+			   
+			   }).fail(function(xhr, status, err) {
+			   	console.log('failed');
+			   });
+			}
     	});
 	});
 
